2af7c8064487938308966fcc018df1c45fc0c88b
scomjoo <scomjoo@gmail.com>
2026-06-10 17:36:54 +0900

    기본 세팅

---
+++ src/test/java/com/example/hello/HelloApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloApplicationTests {
+
+
+}
